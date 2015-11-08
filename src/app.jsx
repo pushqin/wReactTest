7d7daeeb72b51bda4642d0ca18b20d5da004a85f
alexgo <pushqin2@gmail.com>
2015-11-08 15:28:11 +0200

    Added bananareact

--- src/app.jsx
+++ src/app.jsx
@@ -1,10 +1,114 @@
 var React = require("react");
+var ReactDOM = require("react-dom");
+var PyUI = require('reactivebanana');
 
+
+// Entry point
 var App  = React.createClass({
-
     render(){
-        return <h1>hello</h1>;
+        return <div>
+            <TitleRow />
+            <TitleStatus />
+            <FormDescriptionRow />
+            <FormHeader />
+            <FormSubHeader />
+        </div>
     }
-})
+});
 
-React.render(<App />,document.getElementById("example"));
+
+//  Title Row
+var TitleRow  = React.createClass({
+    render(){
+        return <div>
+            <TitleDescription />
+            <TitleStatus />
+        </div>;
+    }
+});
+
+var TitleDescription = React.createClass({
+    render(){
+        return <div>
+            <PyUI.Resource resKey="titleDescription"/>
+        </div>;
+    }
+});
+
+var TitleStatus = React.createClass({
+    render(){
+        return <div>
+            <TitleStatusText />
+            <TitleStatusValue />
+        </div>;
+    }
+});
+
+var TitleStatusText = React.createClass({
+    render(){
+        return <div>
+            <PyUI.Resource resKey="titleStatusText"/></div>;
+    }
+});
+
+var TitleStatusValue = React.createClass({
+    render(){
+        return <div>
+            <Status resKey="statusText"/></div>;
+    }
+});
+
+var Status= React.createClass({
+    getInitialState: function() {
+        return {
+            resKeyValue: 0
+        };
+    },
+    propTypes: {
+        resKey: React.PropTypes.oneOf(['submitted', 'notSubmitted']).isRequired
+    },
+    render(){
+        var resKeyStatus = this.props.resKey + this.state.resKeyValue;
+
+        return <div>
+            <PyUI.Resource resKey={resKeyStatus}/></div>;
+    }
+});
+
+
+
+// Form Description Row
+
+var FormDescriptionRow= React.createClass({
+    render(){
+        return <div>
+            <PyUI.Resource resKey="formDescriptionRow"/></div>;
+    }
+});
+
+// Form Header
+var FormHeader= React.createClass({
+    render(){
+        return <div>
+            <PyUI.Resource resKey="formHeader"/></div>;
+    }
+});
+
+// Form Sub Header
+
+// Form Header
+var FormSubHeader= React.createClass({
+    render(){
+        return <div>
+            <PyUI.Resource resKey="formDescriptionRow"/></div>;
+    }
+});
+
+
+
+
+
+
+
+ReactDOM.render(<App />,
+    document.getElementById("app"));
